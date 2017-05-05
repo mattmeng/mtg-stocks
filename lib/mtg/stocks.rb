ef4545ddc64d903f04e57938bcf94d9272de8838
Matt Meng <matt.meng@intel.com>
2017-05-05 15:16:40 -0600

    Adding more data to be parsed from call to mtg stocks.

--- lib/mtg/stocks.rb
+++ lib/mtg/stocks.rb
@@ -5,18 +5,24 @@ module Mtg
   class Stocks
     MTGSTOCKS_URL = "www.mtgstocks.com"
 
-    def card_price( id )
+    def card_info( id )
       return nil unless id and id > 0
 
       html = Net::HTTP.get( MTGSTOCKS_URL, "/cards/#{id}" )
       return nil unless html
 
-      return [
-        price( html, :lowprice ),
-        price( html, :avgprice ),
-        price( html, :highprice ),
-        price( html, :foilprice )
-      ]
+      tcg_id, card_name = tgc_id_and_name( html )
+      set_name_s = set_name( html )
+
+      return {
+        low_price: price( html, :lowprice ),
+        average_price: price( html, :avgprice ),
+        high_price: price( html, :highprice ),
+        foil_price: price( html, :foilprice ),
+        tcg_id: tcg_id,
+        card_name: card_name,
+        set_name: set_name_s
+      }
     end
 
     def self.instance
@@ -29,6 +35,14 @@ module Mtg
     end
 
     private
+
+    def tgc_id_and_name( source )
+      return source.scan( /<h2><a target="_blank" href="http:\/\/store\.tcgplayer\.com\/product\.aspx\?partner=MTGSTCKS&amp;id=([\d]+)">(.*?)<\/a>&nbsp;.*?<\/h2>/ ).first
+    end
+
+    def set_name( source )
+      return source.scan( /<a href="\/sets\/[\d]+">(.*?)<\/a><\/h5>/ ).first[0]
+    end
 
     def price( source, type )
       rtnval = source.scan( /Prices of last 10 records.*?#{type}['"]>\$?(.*?)<\/td>/m )
